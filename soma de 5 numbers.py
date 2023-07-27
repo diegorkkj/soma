a68b34728909c8a0cfbb536446b694104545a500
diegorkkj <diegosenaisp@gmail.com>
2023-07-27 08:40:40 -0300

    upload soma

---
+++ soma de 5 numbers.py
@@ -0,0 +1,21 @@
+import os
+vari = []
+
+for vari in range(5):
+    numm = float(input("Digite os 5 numros que deseja somar: "))
+    vari.append(numm)
+
+soma = 0
+
+for numm in vari:
+    numm + soma
+    print(soma)
+
+
+    
+
+
+os.system("pause")
+
+
+
